7f90fea302bc45c36c9e9ea8a6145f672f7dbbc0
seong <ree7243@gmail.com>
2023-08-06 23:32:51 +0900

    was 만들기

---
+++ 1/was-pratice/src/test/java/org/example/QueryStringsTest.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueryStringsTest {
+    QueryStrings queryStrings = new QueryStrings("operand1=11&operator=*&operand2=55");
+}
