d367d47b8c9e7480939d3b5cbe891a2d94659e42
seong <ree7243@gmail.com>
2023-08-07 18:18:24 +0900

    동기화로 해결

--- 1/was-pratice/src/main/java/org/example/count/Counter.java
+++ 1/was-pratice/src/main/java/org/example/count/Counter.java
@@ -20,10 +20,11 @@ public class Counter implements Runnable {
 
     @Override
     public void run() {
-        this.increment();
-        System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue()); // 1
-        this.decrement();
-        System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());  // 0
-
+        synchronized (this) {
+            this.increment();
+            System.out.println("Value for Thread After increment " + Thread.currentThread().getName() + " " + this.getValue()); // 1
+            this.decrement();
+            System.out.println("Value for Thread at last " + Thread.currentThread().getName() + " " + this.getValue());  // 0
+        }
     }
 }
